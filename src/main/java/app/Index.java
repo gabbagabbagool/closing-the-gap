0959ee0c73a2cc12cf1cad0c598fcdab2bde38db
tmonaghan-rmit <s3541313@student.rmit.edu.au>
2021-10-15 15:41:24 +1100

    html+= formatting

--- src/main/java/app/Index.java
+++ src/main/java/app/Index.java
@@ -28,6 +28,9 @@ public class Index implements Handler {
         html += "<head>" + 
                "<title>Homepage</title>";
 
+        // Bootstrap's CSS
+        html += "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>";
+        
         // Add some CSS (external file)
         html += "<link rel='stylesheet' type='text/css' href='common.css' />";
 
@@ -62,6 +65,10 @@ public class Index implements Handler {
         // Finish the List HTML
         html += "</ul>";
 
+        // Finish the HTML webpage, import Bootstraps scripts
+        html += "</body>";
+        html += "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p' crossorigin='anonymous'></script>";
+        html += "</html>";
 
         // DO NOT MODIFY THIS
         // Makes Javalin render the webpage
