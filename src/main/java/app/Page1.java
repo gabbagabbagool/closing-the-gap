df63324a9443387a204a5d5e45c9c2e7c4fcd5f4
tmonaghan-rmit <s3541313@student.rmit.edu.au>
2021-10-15 15:42:03 +1100

    adds bootstraps css

--- src/main/java/app/Page1.java
+++ src/main/java/app/Page1.java
@@ -30,6 +30,9 @@ public class Page1 implements Handler {
         html += "<head>" + 
                "<title>Movies</title>";
 
+        // Add Bootsrap's CSS
+        html += "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>";
+
         // Add some CSS (external file)
         html += "<link rel='stylesheet' type='text/css' href='common.css' />";
 
