b0a53a32cfd91cb922b5348b58a69e3266910cfa
tmonaghan-rmit <s3541313@student.rmit.edu.au>
2021-10-18 15:18:24 +1100

    implements hashmap method
    Co-authored-by: Git-Jake-Hill <Git-Jake-Hill@users.noreply.github.com>

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 /**
  * Class for Managing the JDBC Connection to a SQLLite Database.
@@ -126,6 +127,66 @@ public class JDBCConnection {
                 // For now we will just store the movieName and ignore the id
                 y12LGA.add(LgaName);
                 y12LGA.add(y12Count);
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+
+        // Finally we return all of the movies
+        return y12LGA;
+    }
+    
+    public HashMap<String, Integer> getLgaY12IndigHashMap() {
+        HashMap<String, Integer> y12LGA = new HashMap<String, Integer>();
+
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // The Query
+            String query = "SELECT s.lga_code16 AS Code, LGAs.lga_name16 AS Name, SUM(s.count) AS Indig_Y12 " +
+                        "FROM SchoolStatistics AS s JOIN LGAs ON s.lga_code16 = LGAs.lga_code16 " +
+                        "WHERE s.School = 'y12_equiv' AND s.indigenous_status = 'indig' " +
+                        "GROUP BY s.lga_code16, s.indigenous_status, s.School;";
+            
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+
+            // Process all of the results
+            // The "results" variable is similar to an array
+            // We can iterate through all of the database query results
+            while (results.next()) {
+                // We can lookup a column of the a single record in the
+                // result using the column name
+                // BUT, we must be careful of the column type!
+                
+                String  LgaName      = results.getString("Name");
+                String  y12CountTemp = results.getString("Indig_Y12");
+                Integer y12Count     = Integer.parseInt(y12CountTemp);
+
+                // For now we will just store the movieName and ignore the id
+                y12LGA.put(LgaName, y12Count);
             }
 
             // Close the statement because we are done with it
