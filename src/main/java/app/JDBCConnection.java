b132e2878b63833c8a6cd68d7fd10121639c9fca
Jake Hill <s3870682@student.rmit.edu.au>
2021-10-19 12:16:38 +1100

    added function outcome6Lga

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -338,4 +338,66 @@ public class JDBCConnection {
         // Finally we return all of the movies
         return myValue;
     }
+
+
+
+    public HashMap<String, Integer> outcome6Lga() {
+        HashMap<String, Integer> out6LGA = new HashMap<String, Integer>();
+
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // The Query
+            String query = "SELECT q.lga_code16 AS Code, LGAs.lga_name16 AS Name, SUM(q.count) AS Indig_Qual" +
+            "FROM QualificationStatistics AS q JOIN LGAs ON q.lga_code16 = LGAs.lga_code16" +
+            "WHERE q.indigenous_status = 'indig'" +
+            "GROUP BY q.lga_code16;";
+            
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+
+            // Process all of the results
+            // The "results" variable is similar to an array
+            // We can iterate through all of the database query results
+            while (results.next()) {
+                // We can lookup a column of the a single record in the
+                // result using the column name
+                // BUT, we must be careful of the column type!
+                
+                String  LgaName      = results.getString("Name");
+                String  out6CountTemp = results.getString("Indig_Qual");
+                Integer out6Count     = Integer.parseInt(out6CountTemp);
+
+                // For now we will just store the movieName and ignore the id
+                out6LGA.put(LgaName, out6Count);
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+
+        // Finally we return all of the movies
+        return out6LGA;
+    }
 }
