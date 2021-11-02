c972004c02859392b2fde0bab9e64a89b4f77f62
Jake Hill <s3870682@student.rmit.edu.au>
2021-11-02 16:22:51 +1100

    addded filterHookUp method for level3Filter page

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -100,6 +100,79 @@ public class JDBCConnection {
             }
         }
     }    
+
+    /***
+     *
+     */
+    public void filterHookUp(ArrayList<filterOutcomes> OutcomeList, String inputQuery, String outcomeNumAndType) {
+
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // Get Result
+            ResultSet results = statement.executeQuery(inputQuery);
+
+            // Process all of the results
+            // The "results" variable is similar to an array
+            // We can iterate through all of the database query results
+            while (results.next()) {
+
+                // Store the results of this query
+                int     lgaCode = results.getInt("areaCode");
+                String  value = results.getString("value");
+                String  proportion = results.getString("proportion");
+                String  lgaName = results.getString("areaName");
+                String  lgaPopulation = results.getString("pValue");
+                boolean  found = false;
+
+                for (filterOutcomes entry : OutcomeList) {
+                    if (entry.areaCode == lgaCode){
+                        entry.setOutcomes(outcomeNumAndType, value, proportion, lgaPopulation);
+                        entry.setGap();
+                        found = true;
+                    }
+                }
+
+                if (found == false){
+                    // Create a new OutcomeTracker object and set the appropriate values
+                    filterOutcomes myObject = new filterOutcomes();
+                    myObject.setOutcomes(outcomeNumAndType, value, proportion, lgaPopulation);
+                    myObject.setGap();
+                    myObject.areaCode = lgaCode;
+                    myObject.areaName = lgaName;
+                    
+                    // Add this OutcomeTracker object to the methods ArrayList
+                    OutcomeList.add(myObject);
+                }
+
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+    }  
+
     /**
      * create a view in the SQL database
      */
