133ccd59757fad3982d3bb679cb7f669ba41b88d
tmonaghan-rmit <s3541313@student.rmit.edu.au>
2021-11-03 21:04:57 +1100

    outcome 8 done for usability

--- src/main/java/app/level2State.java
+++ src/main/java/app/level2State.java
@@ -99,7 +99,7 @@ public class level2State implements Handler {
             // If the raw radio is selected
             if (model.get("radio").equals("r")){
                 // We ask the database to return a raw count
-                inputQuery = "SELECT SUM(count) AS value, substr(EmploymentStatistics.lga_code16, 1, 1) AS areaCode, State.stateName as areaName FROM EmploymentStatistics JOIN State ON substr(EmploymentStatistics.lga_code16, 1, 1) = stateCode WHERE indigenous_status = 'indig' AND  Labour_force == 'in_lf_unemp' GROUP BY substr(EmploymentStatistics.lga_code16, 1, 1); ";
+                inputQuery = "SELECT sum(employed_indig) as value, substr(lga_code16, 1, 1) as areaCode, State.stateName as areaName FROM labour_force_indig JOIN State on substr(lga_code16, 1, 1) = stateCode GROUP BY stateName;";
             }
             else{
                 inputQuery = "SELECT ROUND(100 - (100.0 * unemployed_indig/employed_indig),1) AS value, substr(lga_code16, 1, 1) as areaCode, State.stateName as areaName FROM unemployed_indig NATURAL JOIN labour_force_indig JOIN State on substr(lga_code16, 1, 1) = stateCode GROUP BY areaName;";
